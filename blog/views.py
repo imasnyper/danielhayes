a2a86f7cd627cf2fe16be46530bbf103be6af7a3
imasnyper <danihaye@gmail.com>
2017-03-18 22:12:42 -0400

    styling changes for header and blog. made header responsive and mobile first with menu button apearing on smaller screens.

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['latest_posts'] = Post.objects.all().order_by('-pub_date')
+        context['latest_posts'] = Post.objects.all().order_by('-pub_date')[:5]
 
         return context
 

fe24f2274b587e1bc83d771c5344ca41790dfac9
imasnyper <danihaye@gmail.com>
2017-03-25 17:58:37 -0400

    successfully added pagination to blog index. currently displays latest 5 blog posts with optional pagination navigation at the bottom of the page.

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 # Create your views here.
 from .models import Post
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "blog/blog_index.html"
+    queryset = Post.objects.all().order_by('-pub_date')
+    context_object_name = 'latest_posts'
+    paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['latest_posts'] = Post.objects.all().order_by('-pub_date')[:5]
 
-        return context
+
+# class IndexView(TemplateView):
+#     template_name = "blog/blog_index.html"
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(IndexView, self).get_context_data(**kwargs)
+#         context['latest_posts'] = Post.objects.all().order_by('-pub_date')[:5]
+#
+#         return context
 
 
 class DetailView(DetailView):

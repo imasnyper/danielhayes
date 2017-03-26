99d3f251f8f276c3e814fa75730cade058ee98ff
imasnyper <danihaye@gmail.com>
2017-03-26 12:45:49 -0400

    using an extra layer of generic templates, so that archive viewing and regular blog viewing are a unified experience. getting pagination working was a challenge.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', views.ArchiveView.as_view(), name='archive'),
+    url(r'^(?P<year>\d{4})/$', views.ArchiveView.as_view(), name='archive'),
     url(r'^(?P<slug>[a-zA-Z0-9\-\_]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.utils import timezone
 
 # Create your views here.
 from .models import Post
@@ -9,19 +10,36 @@ from .models import Post
 class IndexView(ListView):
     template_name = "blog/blog_index.html"
     queryset = Post.objects.all().order_by('-pub_date')
-    context_object_name = 'latest_posts'
+    context_object_name = 'posts'
     paginate_by = 5
 
 
+class ArchiveView(ListView):
+    template_name = "blog/blog_archive.html"
+    context_object_name = 'posts'
+    paginate_by = 5
 
-# class IndexView(TemplateView):
-#     template_name = "blog/blog_index.html"
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(IndexView, self).get_context_data(**kwargs)
-#         context['latest_posts'] = Post.objects.all().order_by('-pub_date')[:5]
-#
-#         return context
+    def get_context_data(self, **kwargs):
+        context = super(ArchiveView, self).get_context_data(**kwargs)
+        if 'year' in self.kwargs.keys() and 'month' in self.kwargs.keys():
+            context.update(year=self.kwargs['year'], month=self.kwargs['month'])
+        elif 'year' in self.kwargs.keys():
+            context.update(year=self.kwargs['year'])
+
+        return context
+
+    def get_queryset(self):
+        if 'month' in self.kwargs:
+            month = str(self.kwargs['month'])
+            next_month = str(int(month) + 1)
+            year = str(self.kwargs['year'])
+            queryset = Post.objects.filter(pub_date__range=("%s-%s-01 00:00" % (year, month), "%s-%s-01 00:00" % (year, next_month))).order_by('pub_date')
+
+        elif 'year' in self.kwargs:
+            year = str(self.kwargs['year'])
+            next_year = str(int(year) + 1)
+            queryset = Post.objects.filter(pub_date__range=("%s-01-01 00:00" % year, "%s-01-01 00:00" % next_year)).order_by('pub_date')
+        return queryset
 
 
 class DetailView(DetailView):

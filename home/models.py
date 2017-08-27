32edb85006c5e930b1877e8f2da94137b88dac78
imasnyper <danihaye@gmail.com>
2017-08-27 10:58:22 -0400

    copied the home page from hazelspace into this project, and incorporated the navbar from the old home page to the new one. Lots of styling to fix up still.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blurb(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    order_num = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,16 +1,62 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.utils import timezone
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.loader import get_template
 
-from polls.models import Question
+from .forms import ContactForm
+from .models import Blurb
 
-import datetime
 
 # Create your views here.
 def index(request):
-    last_question = []
-    active_questions = []
-    last_question = Question.objects.filter(expiry_date__lte=timezone.now())[0]
-    active_questions = Question.objects.filter(expiry_date__gt=timezone.now())
-    context = {'last_question': last_question,
-               'active_questions': active_questions,}
+    blurbs = Blurb.objects.order_by("order_num")
+    form_class = ContactForm
+
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', "")
+            contact_email = request.POST.get('contact_email', "")
+            message = request.POST.get('message', "")
+
+            template = get_template('home/contact_template.txt')
+
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_message': message,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "dhayes" + "",
+                ['danihaye@gmail.com'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            return redirect('index')
+
+    context = {'blurbs': blurbs,
+               'form': form_class}
     return render(request, 'home/index.html', context)
+
+# from django.shortcuts import render, get_list_or_404, get_object_or_404
+# from django.utils import timezone
+#
+# from polls.models import Question
+#
+# import datetime
+#
+# # Create your views here.
+# def index(request):
+#     last_question = []
+#     active_questions = []
+#     last_question = Question.objects.filter(expiry_date__lte=timezone.now())[0]
+#     active_questions = Question.objects.filter(expiry_date__gt=timezone.now())
+#     context = {'last_question': last_question,
+#                'active_questions': active_questions,}
+#     return render(request, 'home/index_old.html', context)

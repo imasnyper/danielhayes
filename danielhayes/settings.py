61b1c5751ce0bc24d8ededf3d2c81cfa01149f87
imasnyper <danihaye@gmail.com>
2017-09-16 19:01:13 -0400

    added github button, removed main wrapper around the home template

--- danielhayes/settings.py
+++ danielhayes/settings.py
@@ -28,7 +28,7 @@ with open(os.path.join(BASE_DIR, 'danielhayes/db_pass.txt')) as f:
     DB_PASS = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 

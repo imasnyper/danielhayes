917d6e273df61a1e660375715012468841845c6e
imasnyper <danihaye@gmail.com>
2017-09-16 23:33:06 -0400

    fixing gunicorn server script

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -8,4 +8,4 @@ GROUP=root
 cd /opt/danielhayes
 source /root/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec gunicorn -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
+exec gunicorn -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE danielhayes.wsgi
